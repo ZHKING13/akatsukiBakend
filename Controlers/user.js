491e2db451a55994343720a6dcf34560ea6c5a61
Zetrey13 <kotaineserge@gmail.com>
2023-07-02 13:59:46 +0000

    fixe bcrypt error

--- Controlers/user.js
+++ Controlers/user.js
@@ -1,5 +1,5 @@
 const userModel = require("../Models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
 const dotenv = require('dotenv').config()
 const { TOKEN_KEY } = process.env
